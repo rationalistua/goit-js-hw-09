f1ed1a0790d6962fd4e6ef37783130bcbdbb9b7a
rationalistua <67971612+rationalistua@users.noreply.github.com>
2023-02-06 15:42:44 +0000

    Deploying to gh-pages from @ rationalistua/goit-js-hw-09@7508c72e8a699311468a4c532d554a6400677dcb 🚀

---
+++ 01-color-switcher.969eeac9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled","true"),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));let d=null;e.setAttribute("disabled","true");
+//# sourceMappingURL=01-color-switcher.969eeac9.js.map
